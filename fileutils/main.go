71c9f489e9245450d134e3f79414efdab761522f
Vikram Vaswani <vikram@dagger.io>
2024-02-02 23:12:28 +0530

    Fixes
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- fileutils/main.go
+++ fileutils/main.go
@@ -6,21 +6,21 @@ import (
 
 type Fileutils struct{}
 
-// example usage: "dagger call grep-dir --directory . --pattern xyz"
-func (m *Fileutils) GrepDir(ctx context.Context, directory *Directory, pattern string) (string, error) {
+// example usage: "dagger call grep-dir --dir . --pattern xyz"
+func (m *Fileutils) GrepDir(ctx context.Context, dir *Directory, pattern string) (string, error) {
 	return dag.Container().
 		From("alpine:latest").
-		WithMountedDirectory("/mnt", directoryArg).
+		WithMountedDirectory("/mnt", dir).
 		WithWorkdir("/mnt").
 		WithExec([]string{"grep", "-R", pattern, "."}).
 		Stdout(ctx)
 }
 
-// example usage: "dagger call tree --directory ."
-func (m *Fileutils) Tree(ctx context.Context, directory *Directory) (string, error) {
+// example usage: "dagger call tree --dir ."
+func (m *Fileutils) Tree(ctx context.Context, dir *Directory) (string, error) {
 	return dag.Container().
 		From("alpine:latest").
-		WithMountedDirectory("/mnt", directoryArg).
+		WithMountedDirectory("/mnt", dir).
 		WithWorkdir("/mnt").
 		WithExec([]string{"tree"}).
 		Stdout(ctx)
