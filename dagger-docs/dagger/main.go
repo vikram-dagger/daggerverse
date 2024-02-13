4298a4aae6c2adf4dffe9c6dfa48f2eec333a335
Vikram Vaswani <vikram@dagger.io>
2024-02-13 20:08:44 +0000

    Fixes
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- dagger-docs/dagger/main.go
+++ dagger-docs/dagger/main.go
@@ -9,22 +9,19 @@ import (
 type DaggerDocs struct {}
 
 // example usage
-// dagger -m github.com/vikram-dagger/daggerverse/dagger-docs call deploy --source . --project vikram-experiments --location us-central1 --repository vikram-test --credential env:GOOGLE_CREDENTIAL
+// dagger -m github.com/vikram-dagger/daggerverse/dagger-docs call deploy --source ./dagger --project vikram-experiments --location us-central1 --repository vikram-test --credential env:GOOGLE_CREDENTIAL
 func (m *DaggerDocs) Deploy(source *Directory, project string, location string, repository string, credential *Secret) (string, error) {
 
 	ctx := context.Background()
 
-	build, err := dag.Container().
+	build := dag.Container().
 		From("node:21").
 		WithDirectory("/home/node", source).
 		WithWorkdir("/home/node/docs").
 		WithMountedCache("/src/node_modules", dag.CacheVolume("node-21-modules")).
-		//WithExec([]string{"npm", "install"}).
-		WithExec([]string{"ls", "-al"}).
-		Stdout(ctx)
-		//WithExec([]string{"npm", "install"}).
-		//WithExec([]string{"npm", "run", "build"}).
-		//Directory("./build")
+		WithExec([]string{"npm", "install"}).
+		WithExec([]string{"npm", "run", "build"}).
+		Directory("./build")
 
 	registry := fmt.Sprintf("%s-docker.pkg.dev/%s/%s/dagger-docs", location, project, repository)
 	split := strings.Split(registry, "/")
